03391bb49e60ee7011cfd7dcd86168ff75be2700
Ruyano Gabriel A. de Resende <ruyano.resende@cedrotech.com>
2018-12-20 16:45:04 -0200

    melhorias no xml do item da lista

--- app/src/main/java/br/com/udacity/ruyano/filmesfamosos/util/CustomViewBindings.java
+++ app/src/main/java/br/com/udacity/ruyano/filmesfamosos/util/CustomViewBindings.java
@@ -1,6 +1,8 @@
 package br.com.udacity.ruyano.filmesfamosos.util;
 
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -20,4 +22,16 @@ public class CustomViewBindings {
     public static void bindRecyclerViewAdapter(ImageView imageView, String imageUrl) {
         GlideUtil.loadImage(imageView, imageUrl, ImageQuality.MEDIUM);
     }
+
+    @BindingAdapter("booleanVisibility")
+    public static void bindLinearLayoutBolleanVisibility(View view, Boolean booleanVisibility) {
+        view.setVisibility(booleanVisibility ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("booleanVisibility")
+    public static void usersEvaluationViewSetValue(UsersEvaluationView view, Boolean booleanVisibility) {
+        view.setVisibility(booleanVisibility ? View.VISIBLE : View.GONE);
+    }
+
+
 }
